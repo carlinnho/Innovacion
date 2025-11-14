d293ed40a93ef0060c89907b42b40c2fc9899f2f
Carlinnho <carlospure2011@gmail.com>
2025-11-13 22:57:26 -0500

    actualizacion de prueba de mi perfil: se cambio 404 a 505

--- frontend/src/pages/Miperfil.jsx
+++ frontend/src/pages/Miperfil.jsx
@@ -6,9 +6,9 @@ export default function Miperfil() {
     <div className="min-h-screen bg-gray-50 flex items-center justify-center">
       <div className="text-center">
         <Wrench size={64} className="mx-auto mb-4 text-orange-600" />
-        <h1 className="text-6xl font-bold text-orange-600 mb-4">404</h1>
+        <h1 className="text-6xl font-bold text-orange-600 mb-4">505</h1>
         <p className="text-lg text-gray-700 max-w-md mx-auto">
-          error 404, la pagina esta en proceso de creación, espere para proximas versiones profesor, estamos trabajando en ello, robert chambea
+          error 505, la pagina esta en proceso de creación, espere para proximas versiones profesor, estamos trabajando en ello, robert chambea
         </p>
       </div>
     </div>
